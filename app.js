d9188ab702ca624375bc04c99393b75f49d1a1cb
daaaduu <tatishvilid3@gmail.com>
2024-11-23 01:33:57 +0400

    .

---
+++ app.js
@@ -0,0 +1,18 @@
+const myNumber = 18;
+const myString = "hello world";
+const myBoolean = true;
+const myNull = null;
+let myUndefined;
+
+// console.log(myNumber);
+// console.log(myString);
+// console.log(myBoolean);
+// console.log(myNull);
+// console.log(myUndefined);
+
+const name = "daviti",
+  dayOfWeek = "saturday",
+  favoriteActivity = "sleeping";
+
+const sentence = `hi my name is${name}! today is ${dayOfWeek}, perfect day for${favoriteActivity}`;
+console.log(sentence);
